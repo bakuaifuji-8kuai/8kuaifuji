3ef44635a1ce7664784d31d223a9bea640c12de5
deploy <deploy@example.com>
2026-09-16 16:26:02 +0800

    Updates

---
+++ assets/swimlanesDiagram-G3AALYLV-DZZNf_SK.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-xomncHUj.js";import{_ as a}from"./index-BiC3DtvH.js";import"./chunk-5VM5RSS4-Bh6sKrPA.js";import"./chunk-XXDRQBXY-Ct7rX-va.js";import"./chunk-VR4S4FIN-ugly7uxr.js";import"./chunk-32BRIVSS-DXX3z2GR.js";import"./channel-DDWa7y4t.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
